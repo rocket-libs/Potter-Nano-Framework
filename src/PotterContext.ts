b74a52f885bdeed889c42a6dc2acbb9514896f51
nyingi <nyingi.maina@gmail.com>
2020-05-20 20:21:55 +0300

    Adding testing

--- src/PotterContext.ts
+++ src/PotterContext.ts
@@ -7,4 +7,16 @@ export class PotterContext<TRepository,TModel>{
         this.repository = repository;
         this.model = model;
     }
+
+    insert<TObject>(object: TObject) {
+        const repositoryObj = this.repository as unknown as object;
+        const candidateKeys = Reflect.ownKeys(repositoryObj);
+        for (const key of candidateKeys) {
+            const candidateProperty = Reflect.get(repositoryObj,key) as TObject[];
+            if(candidateProperty && Array.isArray(candidateProperty)){
+                candidateProperty.push(object);
+                return;
+            }
+        }
+    }
 }
---
+++ tests/context.test.ts
@@ -0,0 +1,34 @@
+import { PotterContext } from "../dist/PotterContext";
+
+
+class TestModel{
+    name: string = "";
+}
+
+class NotModel{
+    name: string = "";
+}
+
+class TestRepo{
+    notModes : NotModel[];
+    models: TestModel[];
+    numbers: number[];
+    stringValue: string;
+}
+
+class Context extends PotterContext<TestRepo,TestModel>{
+
+}
+
+const insertWorks = () => {
+    const name = "Alpha";
+    const context = new Context(new TestRepo(),new TestModel());
+    const newModel = new TestModel();
+    newModel.name = name;
+    context.insert(newModel);
+    
+    expect(context.repository.models.length).toBe(1);
+    expect(context.repository.models[0].name).toBe(name)
+}
+
+test("Insert works",insertWorks);
