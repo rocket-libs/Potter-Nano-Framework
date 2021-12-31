1dd109ada0ad36507c76067b3654ab6e8ed36b9e
nyingi <nyingi.maina@gmail.com>
2021-12-31 12:09:52 +0300

    Allowed for setting of context and potter in state

--- src/PotterState.ts
+++ src/PotterState.ts
@@ -6,7 +6,15 @@ export default abstract class PotterState<TRepository, TModel> {
   protected potter: IPotter<TRepository, TModel> = {} as IPotter<TRepository, TModel>;
 
   public initialize(context: PotterContext<TRepository, TModel>, potter: IPotter<TRepository, TModel>) {
-    this.context = context;
+    this.setContext(context);
+    this.setPotter(potter);
+  }
+
+  public setPotter(potter: IPotter<TRepository, TModel>) {
     this.potter = potter;
+  }
+
+  public setContext(context: PotterContext<TRepository, TModel>) {
+    this.context = context;
   }
 }
